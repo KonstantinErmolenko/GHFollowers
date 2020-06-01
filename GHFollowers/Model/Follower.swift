4eb587f79b11320ae13e88b6ea07d312c2ae6190
Konstantin Ermolenko <19symbols@gmail.com>
2020-06-01 22:45:46 +0300

    Created model objects (Follower and User)

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Ермоленко Константин on 01.06.2020.
+//  Copyright © 2020 Ермоленко Константин. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
