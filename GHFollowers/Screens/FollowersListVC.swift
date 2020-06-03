8dfb17315d093c712959e2288b6eae1a56463287
Konstantin Ermolenko <19symbols@gmail.com>
2020-06-03 09:51:53 +0300

    UICollectionView - FlowLayout

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -34,11 +34,25 @@ class FollowersListVC: UIViewController {
     
     func configueCollectionView() {
         collectionView = UICollectionView(frame: view.bounds,
-                                          collectionViewLayout: UICollectionViewFlowLayout())
+                                          collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemRed
         collectionView.register(FollowerCell.self,
                                 forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width                = view.bounds.width
+        let padding: CGFloat     = 12
+        let itemSpacing: CGFloat = 10
+        let availableWidth       = width - (padding * 2) - (itemSpacing * 2)
+        let itemWidth            = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
     
     // MARK: - Data recieving
