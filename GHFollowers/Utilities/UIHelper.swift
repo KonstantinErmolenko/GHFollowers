8bb65bce94125828c809b12872584696a17d570a
Konstantin Ermolenko <19symbols@gmail.com>
2020-06-04 11:09:19 +0300

    Added Capture Lists in getFollowers() Refactored creating FlowLayout method

---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Ермоленко Константин on 04.06.2020.
+//  Copyright © 2020 Ермоленко Константин. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width                = view.bounds.width
+        let padding: CGFloat     = 12
+        let itemSpacing: CGFloat = 10
+        let availableWidth       = width - (padding * 2) - (itemSpacing * 2)
+        let itemWidth            = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
