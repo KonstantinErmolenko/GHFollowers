880a3db11897391e321cdfbc876a91076a2df9b8
ermolenko_ks <19symbols@gmail.com>
2020-06-21 22:34:57 +0300

    Child View Controller - UserInfoHeaderVC

---
+++ GHFollowers/Custom views/Labels/GFSecondaryTitleLable.swift
@@ -0,0 +1,36 @@
+//
+//  GFSecondaryTitleLable.swift
+//  GHFollowers
+//
+//  Created by Ермоленко Константин on 21.06.2020.
+//  Copyright © 2020 Ермоленко Константин. All rights reserved.
+//
+
+import UIKit
+
+class GFSecondaryTitleLable: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configue()
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configue()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configue() {
+        textColor                 = .secondaryLabel
+        minimumScaleFactor        = 0.9
+        lineBreakMode             = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
