b5b3a669004dae98b78aa3e55c3f96543ac1b022
ermolenko_ks <19symbols@gmail.com>
2020-06-24 12:46:39 +0300

    UserInfoVC
    UserInfoVC Cleanup & Setup

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
-    var headerView = UIView()
-
+    var headerView          = UIView()
+    var itemViewOne         = UIView()
+    var itemViewTwo         = UIView()
+    var itemViews: [UIView] = []
+    
     var username: String!
     
     override func viewDidLoad() {
@@ -19,7 +22,7 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         configueNavigationBar()
         layoutUI()
-        populateUserInfo()
+        getUserInfo()
     }
     
     func configueNavigationBar() {
@@ -36,7 +39,7 @@ class UserInfoVC: UIViewController {
         dismiss(animated: true)
     }
     
-    func populateUserInfo() {
+    func getUserInfo() {
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             
@@ -54,15 +57,35 @@ class UserInfoVC: UIViewController {
     }
     
     func layoutUI() {
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat    = 20
+        let itemHeight: CGFloat = 140
         
+        let itemViews = [headerView, itemViewOne, itemViewTwo]
+
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+        }
+        itemViewOne.backgroundColor = .systemBlue
+        itemViewTwo.backgroundColor = .systemRed
+
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight)
         ])
+        
+        for itemView in itemViews {
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+        }
     }
     
     func add(childVC: UIViewController, to containerView: UIView) {
